e11b2730456ea2415a5c7fa5850c53b8b7219d61
nia1003 <146817249+nia1003@users.noreply.github.com>
2024-08-20 17:13:33 +0800

    berkerley function calling leaderboard added

---
+++ berkeley-function-call-leaderboard/model_handler/model_style.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class ModelStyle(Enum):
+    Gorilla = "gorilla"
+    OpenAI = "gpt"
+    Anthropic = "claude"
+    Mistral = "mistral"
+    Google = "google"
+    FIREWORK_AI = "firework_ai"
+    NEXUS = "nexus"
+    OSSMODEL = "ossmodel"
+    COHERE = "cohere"
